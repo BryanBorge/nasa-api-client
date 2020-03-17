d9fdbc4945be1881b872215c27c45733c85e88a5
Bryan <BryanBorgesano97@gmail.com>
2020-03-17 12:40:11 -0400

    Testing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import APOD from './containers/AstronomyPicOfTheDay/APOD'
+import React from "react";
+import APOD from "./containers/AstronomyPicOfTheDay/APOD";
+import MarsRover from './containers/MarsRover/MarsRover'
+
+
+
 function App() {
   return (
-    <div>
-     <APOD />
-    </div>
+      <div>
+        <APOD />
+        <MarsRover />
+      </div>
   );
 }
 
