e278d300e3a75e67c3bde1fd97bb1600e7e5e0d9
Bryan <BryanBorgesano97@gmail.com>
2020-03-17 12:36:13 -0400

    PictureInfo can now show youtube videos from the image link

--- src/components/PictureInfo/PictureInfo.js
+++ src/components/PictureInfo/PictureInfo.js
@@ -1,22 +1,37 @@
 import React from "react";
-import { DisplayDate } from "../../helper/DisplayDate";
+import ReactPlayer from "react-player";
 import classes from "./PictureInfo.module.css";
+
 const PictureInfo = props => {
+  let options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric"
+  };
+
   let credit = props.copyRight && <p> Image Credit: {props.copyRight}</p>;
   let showInfo = props.loading ? (
     <p> LOADING </p>
   ) : (
     <div className={classes.Info}>
       <h2>{props.title}</h2>
-      <p>{props.Date && DisplayDate(props.date)}</p>
+      <p>{props.date.toLocaleString("en-US", options)}</p>
       <p>{props.desc}</p>
       <div>{credit}</div>
-      <a>
-        <img src={props.picture} alt={props.picture} />
-      </a>
+
+      {props.picture.includes("youtube") ? (
+        <ReactPlayer
+          className={classes.video}
+          url={props.picture}
+          controls={true}
+        />
+      ) : (
+        <img src={props.picture} alt="N/A" />
+      )}
     </div>
   );
-  return <div>{showInfo}</div>;
+  return <div>{!props.success ? showInfo : <p> No Results Found</p>}</div>;
 };
 
 export default PictureInfo;
