c02ac24246a33d7cc386e4823c0ae8dbbef9f34d
Bryan Borgesano <BryanBorgesano97@gmail.com>
2020-05-06 11:35:04 -0400

    Fixed issue of the date being one day behind the chosen date

--- src/components/PictureInfo/PictureInfo.js
+++ src/components/PictureInfo/PictureInfo.js
@@ -2,33 +2,41 @@ import React from "react";
 import ReactPlayer from "react-player";
 import classes from "./PictureInfo.module.css";
 
-const PictureInfo = props => {
+const PictureInfo = (props) => {
   let options = {
     weekday: "long",
     year: "numeric",
     month: "long",
-    day: "numeric"
+    day: "numeric",
   };
 
   let credit = props.copyRight && <p> Image Credit: {props.copyRight}</p>;
   let showInfo = props.loading ? (
     <p> LOADING </p>
   ) : (
-    <div className={classes.Info}>
-      <h2>{props.title}</h2>
-      <p>{props.date.toLocaleString("en-US", options)}</p>
-      <p>{props.desc}</p>
-      <div>{credit}</div>
-
-      {props.picture.includes("youtube") ? (
-        <ReactPlayer
-          className={classes.video}
-          url={props.picture}
-          controls={true}
-        />
-      ) : (
-        <img src={props.picture} alt="N/A" />
-      )}
+    <div>
+      <div className={classes.Info}>
+        <h2>{props.title}</h2>
+        <p>
+          {new Date(props.date.split("-").join("/")).toLocaleDateString(
+            "en-US",
+            options
+          )}
+        </p>
+        <p>{props.desc}</p>
+        <div>{credit}</div>
+      </div>
+      <div>
+        {props.picture.includes("youtube") ? (
+          <ReactPlayer
+            className={classes.video}
+            url={props.picture}
+            controls={true}
+          />
+        ) : (
+          <img src={props.picture} alt="N/A" className={classes.Img} />
+        )}
+      </div>
     </div>
   );
   return <div>{!props.success ? showInfo : <p> No Results Found</p>}</div>;
