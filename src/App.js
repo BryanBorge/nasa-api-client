3f6fc94093bd902cd682ee36e248421c819efea9
Bryan Borgesano <BryanBorgesano97@gmail.com>
2020-08-28 10:56:32 -0400

    Added routes for both pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from "react";
 import APOD from "./containers/AstronomyPicOfTheDay/APOD";
 import MarsRover from "./containers/MarsRover/MarsRover";
+import {Appbar} from "./components/AppBar/Appbar";
+import {MuiPickersUtilsProvider} from "@material-ui/pickers";
+import DateFnsUtils from "@date-io/date-fns";
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <APOD />
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Appbar title="Astronomy Picture Of The Day" />
+              <APOD />
+            </Route>
+            <Route exact path="/mars-rover">
+              <Appbar title="Mars Rover" />
+              <MarsRover />
+            </Route>
+          </Switch>
+        </Router>
+      </MuiPickersUtilsProvider>
     </div>
   );
 }
