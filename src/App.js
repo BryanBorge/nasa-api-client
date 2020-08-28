6054bbf876360b89f8e649bbcf2402d90be98663
Bryan Borgesano <BryanBorgesano97@gmail.com>
2020-08-28 11:22:41 -0400

    Trying to add baseurl to router to fix ghpage not working

--- src/App.js
+++ src/App.js
@@ -7,16 +7,17 @@ import DateFnsUtils from "@date-io/date-fns";
 import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 
 function App() {
+  let baseUrl = "https://bryanborge.github.io/nasa-api-client/";
   return (
     <div>
       <MuiPickersUtilsProvider utils={DateFnsUtils}>
         <Router>
           <Switch>
-            <Route exact path="/">
+            <Route exact path={`${baseUrl}/`}>
               <Appbar title="Astronomy Picture Of The Day" />
               <APOD />
             </Route>
-            <Route exact path="/mars-rover">
+            <Route exact path={`${baseUrl}/mars-rover`}>
               <Appbar title="Mars Rover" />
               <MarsRover />
             </Route>
