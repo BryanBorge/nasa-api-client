45a103567419210d26a9d03280a61a115c71bf59
Bryan Borgesano <BryanBorgesano97@gmail.com>
2020-08-28 11:12:52 -0400

    Updates

---
+++ precache-manifest.fdc15f8d633dd618e937026b1926cfe8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ced3256c334c69e988f038466e0755ba",
+    "url": "/nasa-api-client/index.html"
+  },
+  {
+    "revision": "fcbc0e7cee90caee42ef",
+    "url": "/nasa-api-client/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "41b19005eeab61f74728",
+    "url": "/nasa-api-client/static/css/main.419554da.chunk.css"
+  },
+  {
+    "revision": "fcbc0e7cee90caee42ef",
+    "url": "/nasa-api-client/static/js/2.4d8d7cc1.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/nasa-api-client/static/js/2.4d8d7cc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41b19005eeab61f74728",
+    "url": "/nasa-api-client/static/js/main.3302a48b.chunk.js"
+  },
+  {
+    "revision": "63824b52b52b7b49fe84",
+    "url": "/nasa-api-client/static/js/runtime-main.28221632.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nasa-api/precache-manifest.de980fb9b7c0427580407a63e787a51a.js"
+  "/nasa-api-client/precache-manifest.fdc15f8d633dd618e937026b1926cfe8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nasa-api/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nasa-api-client/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
